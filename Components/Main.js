a51a66ba1c082abb63700fbefae4b345db8960da
Agustin Pereyra <agusabel555@gmail.com>
2022-06-23 22:47:03 -0300

    aaa

--- Components/Main.js
+++ Components/Main.js
@@ -1,46 +1,37 @@
-import * as React from 'react';
-import Hero from '../Components/Hero'
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Grid from '@mui/material/Grid';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-const cards = [1, 2, 3, 4, 5, 6];
+import * as React from "react";
+import Hero from "../Components/Hero";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import Image1 from "../public/aaaa.jpg";
 
-const theme = createTheme();
 
 export default function Main() {
   return (
-    <ThemeProvider theme={theme}>
-      
-        <Hero></Hero>
-
-        <Container sx={{ py: 8 }} maxWidth="md">
-          <Grid container spacing={4}>
-              <Grid item xs={12} sm={6} md={4}>
-                <Card
-                  sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
-                >
-                  <CardMedia
-                    component="img"
-                    image="https://source.unsplash.com/random"
-                    alt="random"
-                  />
-                  <CardContent sx={{ flexGrow: 1 }}>
-                    <Typography gutterBottom variant="h5" component="h2">
-                      Heading
-                    </Typography>
-                    <Typography>
-                      This is a media card. You can use this section to describe the
-                      content.
-                    </Typography>
-                  </CardContent>
-                </Card>
-              </Grid>
+    <>
+      <Hero></Hero>
+      <Container maxWidth="md">
+        <Grid container spacing={4}>
+          <Grid item xs={12} sm={6} md={4}>
+            <Card
+              sx={{ height: "100%", display: "flex", flexDirection: "column" }}
+            >
+              <CardMedia component="img" width="100%" height="250"  image={Image1} alt="image1" />
+              <CardContent sx={{ flexGrow: 1 }}>
+                <Typography gutterBottom variant="h5" component="h2">
+                  Cornalitos
+                </Typography>
+                <Typography>
+                  aaaaaaaaaaaaaaaasfdasfassssssss
+                </Typography>
+              </CardContent>
+            </Card>
           </Grid>
-        </Container>
-    </ThemeProvider>
+        </Grid>
+      </Container>
+    </>
   );
 }
