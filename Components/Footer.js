8fe3cbd27e23113f9ebc95708afc9b70f4f708b7
Agustin Pereyra <agusabel555@gmail.com>
2022-06-26 21:08:05 -0300

    Footer terminado

--- Components/Footer.js
+++ Components/Footer.js
@@ -8,7 +8,7 @@ import Button from '@mui/material/Button';
 export default function Footer() {
   return (
     <>
-      <Box sx={{ bgcolor: "background.paper", p: 6 }} component="footer">
+      <Box sx={{ bgcolor: 'primary.main', p: 6 }} component="footer">
         <Typography variant="h6" align="center" gutterBottom>
           Creado por: Agustin Pereyra. Contacto: @agusabel_
         </Typography>
@@ -21,35 +21,38 @@ export default function Footer() {
           <Container maxWidth="md">
           <Typography variant="h6" align="center" gutterBottom>Formulario de Contacto</Typography>
             <Grid container spacing={4}>
-              <Grid item xs={4} sm={6} md={6}>
+              <Grid item xs={12} sm={6} md={6}>
                 <TextField
                 fullWidth
                   id="filled-basic"
                   label="Nombre y Apellido"
                   variant="filled"
+                  color="grey"
                 />
               </Grid>
 
-              <Grid item xs={4} sm={6} md={6}>
+              <Grid item xs={12} sm={6} md={6}>
                 <TextField
                 fullWidth
                   id="filled-basic"
                   label="Email"
                   variant="filled"
+                  color="grey"
                 />
               </Grid>
 
-              <Grid item xs={4} sm={6} md={12}>
+              <Grid item xs={12} sm={12} md={12}>
                 <TextField
                 fullWidth
                   id="filled-basic"
                   label="Descripcion"
                   variant="filled"
+                  color="grey"
                 />
               </Grid>
 
 
-              <Grid item xs={4} sm={6} md={12}>
+              <Grid item xs={12} sm={12} md={12}>
               <Button variant="contained" color="grey">Enviar</Button>
               </Grid>
             </Grid>
